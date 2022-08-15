a1db6850a850f230a0710512d2433c25057f7228
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2022-08-15 13:48:38 +0900

    Update solution.py
    재귀함수를 사용한 문제풀이

--- Programmers/lev1/최대공약수와 최소공배수/solution.py
+++ Programmers/lev1/최대공약수와 최소공배수/solution.py
@@ -4,4 +4,14 @@ def solution(n, m):
     max = math.gcd(m,n)
     min = int((m*n) / max)
     return max,min
+    
+#solution 2 재귀함수 
+def gcd(n,,m):
+  return m if a%b ==0 else gcd(b,a%b)
+
+def lcm(n,m):
+  return int((n*m)/gcd(n,m))
+
+def solution(n,m):
+  return lcm(n,m), gcd(n,m)
   
