356a66b1c631588291d7717e311c10a1dc53264e
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2022-08-16 12:03:51 +0900

    Update solution.py
    simplify

--- Programmers/lev1/음양 더하기/solution.py
+++ Programmers/lev1/음양 더하기/solution.py
@@ -1,3 +1,8 @@
-def solution(absolutes,signs):
+#solution 1
+def solution1(absolutes,signs):
     signs_= [ -1 if i == False else 1 for i in signs ]
     return sum([i*v for i,v in zip(absolutes, signs_)])
+
+#solution2
+def solution2(absolutes,signs):
+    return sum(a if s else -a for a,s in zip(absolutes,signs))
