84be474c00220bc48257082e7058f79cdb4768ef
hana cho <sjfn242@gmail.com>
2022-08-26 15:40:02 +0900

    update:solution

--- Programmers/lev1/핸드폰 번호 가리기/solution.py
+++ Programmers/lev1/핸드폰 번호 가리기/solution.py
@@ -2,3 +2,6 @@ def solution(phone_number):
     tmp = ["*" for i in range(len(phone_number)-4)]
     return "".join(tmp) + phone_number[-4:]
 
+# 한줄로 풀어보기
+def solution2(phone_number):
+    return "*"* (len(phone_number)-4)+ phone_number[-4:]
