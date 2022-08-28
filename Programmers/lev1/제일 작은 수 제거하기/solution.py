087c074535d34814ec445cb9ff7c083e1e25f63e
hana cho <sjfn242@gmail.com>
2022-08-28 17:13:30 +0900

    add file

--- Programmers/lev1/제일 작은 수 제거하기/solution.py
+++ Programmers/lev1/제일 작은 수 제거하기/solution.py
@@ -1,7 +1,13 @@
 def solution(arr):
     arr.remove(sorted(arr)[0])
     return arr if len(arr)>0 else -1
+# raise an error when empty input is given
 
-#more simple
+#simpler
 def solution2(arr):
     return [i for i in arr if i>min(arr)]
+
+print(solution([]))
+#IndexError
+print(solution2([]))
+#[]
