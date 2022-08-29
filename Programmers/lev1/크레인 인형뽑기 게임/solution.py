4f30e33f56aab1e96a46a5b742b2971805ebd664
hana <sjfn242@gmail.com>
2022-08-29 19:43:03 +0900

    update

--- Programmers/lev1/크레인 인형뽑기 게임/solution.py
+++ Programmers/lev1/크레인 인형뽑기 게임/solution.py
@@ -1,6 +1,5 @@
 def solution(board, moves):  
     new_list = [list(i) for i in zip(*board)]
-    #[(0, 0, 0, 4, 3), (0, 0, 2, 2, 5), (0, 1, 5, 4, 1), (0, 0, 0, 4, 3), (0, 3, 1, 2, 1)]
     tmp = [] 
     cnt = 0
 
@@ -8,18 +7,18 @@ def solution(board, moves):
         for j in range(len(new_list[0])): 
             if new_list[i-1][j] !=0:
                 
-                if len(tmp) > 0 :
-                    if tmp[-1] == new_list[i-1][j]:
-                        cnt+=2
-                        new_list[i-1][j] = 0
-                        tmp.pop(-1)
-                        break
-                    else: 
-                        tmp.append(new_list[i-1][j])
-                        new_list[i-1][j] = 0
-                        break
+                if len(tmp) > 0 and tmp[-1] == new_list[i-1][j]:
+                    cnt+=2
+                    new_list[i-1][j] = 0
+                    tmp.pop(-1)
+                    break
+                
                 else:
                     tmp.append(new_list[i-1][j])
                     new_list[i-1][j] = 0
                     break
     return cnt
+
+b= [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+m =	[1,5,3,5,1,2,1,4]
+print(solution(b,m))
