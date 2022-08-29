7baa25438657b7b543a51f76609f623bae126d84
hana <sjfn242@gmail.com>
2022-08-29 19:43:03 +0900

    update

--- BaekJoon/py/silver4/괄호/solution.py
+++ BaekJoon/py/silver4/괄호/solution.py
@@ -4,10 +4,7 @@ for i in range(num):
     lst = list(input())
     cnt = 0
     for j in lst:
-        if j == '(':
-            cnt += 1
-        elif j == ')':
-            cnt -= 1
+        cnt = cnt+1 if j == '(' else cnt-1 if  j == ')' else cnt
             
         if cnt < 0:
             print('NO')
