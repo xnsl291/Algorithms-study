08998b2d86147d443b075c612da1fc3947f680db
hana <sjfn242@gmail.com>
2022-09-05 17:54:09 +0900

    add: new files

--- Programmers/lev2/프린터/solution.py
+++ Programmers/lev2/프린터/solution.py
@@ -1,15 +1,22 @@
 def solution(priorities, location):
+    queue = [(i,v) for i,v in enumerate(priorities)]
+    cnt = 0
     
-    for i in priorities:
-        if i != max(priorities):
-            priorities.append(i)
-            priorities.pop(0)
-
+    while True:
+        idx = queue.pop(0) #(0,1)
+        
+        if any(idx[1] < q[1] for q in queue):
+            queue.append(idx)    
+        else:
+            cnt+=1
+            if idx[0] == location:
+                return cnt
     
-    return location
+    
+    
+# p = [1, 3, 9, 1, 1, 1]
+# loc = 1
+# print(solution(p,loc))
 
 
 
-p = [1, 1, 9, 1, 1, 1]
-l = 0
-print(solution(p,l))
