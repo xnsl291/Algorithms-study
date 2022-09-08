58cecce9e7c4c357e3d574f599734ac6e40906be
hana cho <sjfn242@gmail.com>
2022-09-08 10:21:30 +0900

    update folders

--- BaekJoon/py/Bronze2/10829_이진수변환/solution2.py
+++ BaekJoon/py/Bronze2/10829_이진수변환/solution2.py
@@ -1,15 +1,31 @@
+# # recursive 
+
+# lst = []
+# def convert(n):
+#     if n//2 != 1 :
+#         n = n//2
+#         lst.insert(0,str(n%2))
+#         convert(n)
+#     else:
+#         lst.insert(0,str(1))
+        
+#     return "".join(lst)
+
+# n = int(input())
+# print(convert(n))
+
+
 # recursive 
 
 lst = []
 def convert(n):
-    if n//2 != 1 :
-        n = n//2
-        lst.insert(0,str(n%2))
-        convert(n)
-    else:
+    
+    lst.insert(0,str(n%2))
+    n = n//2
+    if n ==1:
         lst.insert(0,str(1))
-        
-    return "".join(lst)
+
+    return convert(n) if n//2 >= 1 else "".join(lst)
 
 
 n = int(input())
