550ef566683469a0a8cb73c0d56ad80700a5f010
hana <sjfn242@gmail.com>
2022-11-23 17:49:18 +0900

    fixed pg:py 기사단원 runtimeError

--- Programmers/py/lev1/기사단원의 무기/solution.py
+++ Programmers/py/lev1/기사단원의 무기/solution.py
@@ -1,16 +1,20 @@
-# 시간 초과로 테스트케이스 실패 -- 수정요망 
-
 def solution(number, limit, power):
-    answer = 0
     # 1. number까지 각 수의 약수 개수 세기 
     # 2. limit 초과하는지 확인하고 
     # 3. 초과분은 power로 대체 
     # 4. 합 리턴
-    lst = [] 
+    cnt = 0
     for i in range(1,number+1):
-        cnt = 1
-        for j in range(1,i):
-                cnt+=1 if i%j == 0 else +0
-        lst.append(power if cnt > limit else cnt)
+        cnt += power if getNum(i) > limit else getNum(i)
+    return cnt
 
-    return sum(lst)
+
+def getNum(number):
+    cnt = 0
+    
+    for j in range(1, int(number**(1/2))+1):
+        if number%j == 0 :
+            cnt= cnt+2 if ( (j**2) != number) else cnt+1 
+
+    return cnt
+
