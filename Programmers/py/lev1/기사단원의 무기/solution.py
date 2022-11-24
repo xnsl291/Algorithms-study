fb3ee84c20fd4a19e10454883025dd87667fbdaf
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2022-11-24 18:29:20 +0900

    fix pg:py 기사단원.py

--- Programmers/py/lev1/기사단원의 무기/solution.py
+++ Programmers/py/lev1/기사단원의 무기/solution.py
@@ -5,21 +5,16 @@ def solution(number, limit, power):
     # 3. 초과분은 power로 대체 
     # 4. 합 리턴
     cnt = 0
-    for i in range(1, int (number/2)+1):
-        print(i,"의 약수 개수 구하기")
+    for i in range(1,number+1):
         cnt += power if getNum(i) > limit else getNum(i)
-        print("-------------------")
-
-    return cnt*2
+    return cnt
 
 
 def getNum(number):
     cnt = 0
-    
     for j in range(1, int(number**(1/2))+1):
         if number%j == 0 :
             cnt= cnt+2 if ( (j**2) != number) else cnt+1 
-            print("j = ",j," cnt = ",cnt)
+            
     return cnt
 
-print(solution(5,3,2)) #203
