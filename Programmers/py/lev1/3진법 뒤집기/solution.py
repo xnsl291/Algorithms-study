f8f2c6b82c478a1857a15f0ed9c16123b07b0c09
hana <sjfn242@gmail.com>
2022-12-31 23:45:20 +0900

    fix pg:py 3진법.py

--- Programmers/py/lev1/3진법 뒤집기/solution.py
+++ Programmers/py/lev1/3진법 뒤집기/solution.py
@@ -1,20 +1,18 @@
 def solution(n):
     result , target = 0, 3
     lst = []
-
     # 3진법 구화기 
     while(n > target-1):
-        lst.append((n%target) * pow(target,idx))
+        lst.append( n%target )
         n = n//3
-        idx+=1
-        print("lst : ",lst)
     lst.append(n)
-    
+    print(lst)
     # 10 진법으로 변경
-    # for i in range(len(lst)):
-    #     result += (lst[i] * pow( target , len(lst)-i-1   ) )
+    for i in range(len(lst)):
+        result += (lst[i] * pow( target , len(lst)-i-1   ) )
     
-    return sum(lst)
+    # return sum(lst)
+    return result
 
 
-print(solution(45))
+print("resiult : ",solution(45))
