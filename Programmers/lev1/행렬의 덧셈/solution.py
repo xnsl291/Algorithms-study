0ac77d6ecdbae7f2f5ae10a7b6b83bc3937affba
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2023-09-28 21:42:42 +0900

    Update solution.py
    중첩 zip문 사용

--- Programmers/lev1/행렬의 덧셈/solution.py
+++ Programmers/lev1/행렬의 덧셈/solution.py
@@ -5,4 +5,6 @@ def solution1(arr1,arr2):
             arr1[i][j] += arr2[i][j]
     return arr1
                  
-
+#solution 2 - 1줄로 작성하기/ zip함수 중첩사용
+def solution2(arr1,arr2):
+  return [[i+v for i,v in zip(a1,a2)] for a1,a2 in zip(arr1,arr2)]
