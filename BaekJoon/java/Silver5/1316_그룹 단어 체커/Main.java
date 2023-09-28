7946b83bf25d1799fdcf23dfada0329431547326
hana cho <sjfn242@gmail.com>
2023-09-28 21:45:17 +0900

    modify 1316_그룹단위체커 : string[]생성-> char 비교로 방식변경

--- BaekJoon/java/Silver5/1316_그룹 단어 체커/Main.java
+++ BaekJoon/java/Silver5/1316_그룹 단어 체커/Main.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     private static int check_word(String word)
     {
         Stack stack = new Stack();
-        String [] word_arr = word.split("");
-        
-        for(String i : word_arr)
+        for(int i=0 ; i<word.length() ; i++)
         {
-            if (!stack.contains(i))
-                stack.push(i);
-            else if (!i.equals(stack.lastElement()))
+            char target = word.charAt(i);
+            if (!stack.contains(target))
+                stack.push(target);
+
+            else if (!String.valueOf(stack.lastElement()).equals(String.valueOf(target)))
                 return 0;
         }
         return 1;
     }
-    
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
