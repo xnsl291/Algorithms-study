de1a46adbc15866e1455f4bb22129355f70e19e0
hana cho <sjfn242@gmail.com>
2023-09-28 21:45:30 +0900

    modify bj:java 2167_2차원 배열의 합 : (실행시간 줄이기) Scanner > BufferedReader , StringTokenizer로 변환

--- BaekJoon/java/Silver5/2167_2차원 배열의 합/Main.java
+++ BaekJoon/java/Silver5/2167_2차원 배열의 합/Main.java
@@ -1,38 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        int n, m,x1,y1,x2,y2;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scan.nextInt();
-        int m = scan.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int [][] array = new int[n+1][m+1];
 
-        int [][] array = new int[n][m];
-
-        for (int i =0; i<n;i++)
-            for(int j =0; j<m; j++)
-                array[i][j] = scan.nextInt();
-
+        for (int i =1; i<n+1;i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j =1; j<m+1; j++)
+                array[i][j] = Integer.parseInt(st.nextToken());
+        }
+        
         // 누적합
         for(int i=1; i<n+1; i++) {
-            for(int j=1; j<m+1; j++) 
-                array[i][j] = array[i][j]+array[i-1][j] + array[i][j-1] - array[i-1][j-1];
+            for(int j=1; j<m+1; j++)
+                array[i][j] = array[i][j] + array[i-1][j] + array[i][j-1] - array[i-1][j-1];
         }
-        
-        int sum_num = scan.nextInt();
+        int sum_num = Integer.parseInt(br.readLine());
         for (int i =0; i<sum_num ;i++) {
-            int answer = 0;
-            int x1 = scan.nextInt();
-            int y1 = scan.nextInt();
-            int x2 = scan.nextInt();
-            int y2 = scan.nextInt();
-
-        
-        System.out.println(array[x2][y2] - array[x2][y1-1] - array[x1-1][y2] + array[x1-1][y1-1])
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            System.out.println(array[x2][y2] - array[x1-1][y2] - array[x2][y1-1] + array[x1-1][y1-1]);
         }
-
-
-
 
     }
 }
