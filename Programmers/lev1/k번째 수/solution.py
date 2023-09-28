f5f64f3576f392635687734981f12511d54f2166
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2023-09-28 21:42:56 +0900

    Update solution.py
    simplify the code

--- Programmers/lev1/k번째 수/solution.py
+++ Programmers/lev1/k번째 수/solution.py
@@ -4,3 +4,7 @@ def solution(array, commands):
         start,end,index = i
         result.append(list(sorted(array[start-1:end]))[index-1])
     return result
+
+#1줄코드작성해보기
+def solution2(array, commands):
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands ] 
