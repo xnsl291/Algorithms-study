ab75e2775787e7c73e497537ecc33849a8780e53
hana cho <sjfn242@gmail.com>
2023-09-28 21:44:35 +0900

    add pg:py 숫자의 표현

---
+++ Programmers/py/lev2/숫자의 표현/solution.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = 0 
+    for i in range(1,n+1):
+        sum = 0
+        for j in range(i,n+1):
+            sum+=j
+            if sum == n:
+                answer+=1
+                break
+            elif sum > n:
+                break
+    return answer
+
+# n = 15
+# print(solution(n))
+
+# n = 15
+# print(solution(n))
