ffe5571d3fb071401916164d63c69dab43bc4acc
hana <sjfn242@gmail.com>
2023-09-28 21:43:16 +0900

    update file

--- Programmers/lev1/나누어 떨어지는 숫자 배열/solution.py
+++ Programmers/lev1/나누어 떨어지는 숫자 배열/solution.py
@@ -1,3 +1,8 @@
 def solution(arr, divisor):
     arr= sorted([ i for i in arr if i%divisor ==0 ])
     return [-1] if len(arr) <1 else arr
+
+#or 사용
+def solution(arr, divisor): 
+    return sorted([n for n in arr if n%divisor == 0]) or [-1]
+# 앞의 값이 참일경우 or 앞에만, 거짓일 경우 뒤에것까지 호출
--- Programmers/lev1/소수찾기/solution.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    cnt = 0
-    for i in range(1,n+1):
-        if len([j for j in range(1,i+1) if i % j ==0]) == 2:
-            cnt+=1
-    return cnt
