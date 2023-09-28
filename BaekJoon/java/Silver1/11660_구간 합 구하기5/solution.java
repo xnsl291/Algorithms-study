01e95a3393aaf1553a02fe07c4b2f76a5d865818
hana cho <sjfn242@gmail.com>
2023-09-28 21:45:33 +0900

    add bj:java 11660_구간합구하기5

---
+++ BaekJoon/java/Silver1/11660_구간 합 구하기5/solution.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        int n,m,x1,x2,y1,y2;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int [][] numbers = new int [n][n];
+        int [][] dp = new int [n+1][n+1];
+
+        for (int k=1; k<n+1; k++) {
+            st = new StringTokenizer(br.readLine());
+            for (int p = 1; p < n+1; p++) {
+                numbers[k-1][p-1] = Integer.parseInt(st.nextToken());
+                dp[k][p] = numbers[k-1][p-1] + dp[k-1][p]+ dp[k][p-1]-dp[k-1][p-1]  ;
+            }
+        }
+
+        for (int k =0; k<m ;k++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
+        }
+    }
+}
