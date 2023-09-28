4f337747c206c0209268a4d8d1ab3784ce17f457
hana cho <xnsl291@gmail.com>
2023-09-28 21:43:07 +0900

    add: additional codes

--- Programmers/lev1/최소직사각형/solution.py
+++ Programmers/lev1/최소직사각형/solution.py
@@ -5,3 +5,8 @@ def solution(sizes):
         if row[i]> column[i]:
             row[i],column[i] = column[i],row[i] 
     return max(row)*max(column)
+
+# 리스트 저장대신 값만 반환하는 함수
+# result = 최대값 x 최소값들 중 최대값 
+def solution2(sizes):
+    return max([max(i) for i in sizes])*max([min(i) for i in sizes])
