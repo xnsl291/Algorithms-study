18171b01c6bbf033c9771589f649c5b9dfe79929
hana <sjfn242@gmail.com>
2023-09-28 21:43:19 +0900

    update code

--- BaekJoon/py/Silver2/2164 카드2/solution2.py
+++ BaekJoon/py/Silver2/2164 카드2/solution2.py
@@ -1,9 +1,12 @@
 #solution2 - queue
-#FIFO
 lst = list(range(1,int(input())+1))
 
 while len(lst)>1:
     lst.pop(0)
     lst.append(lst[0])
-    lst.pop(0)
+    # lst.pop(0)
+    
+print(lst[0])
+    
+    #지영님: 슬라이싱 s[2:]
     
