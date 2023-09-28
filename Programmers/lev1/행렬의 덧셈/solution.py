271342cf0d990cb10efd2892aae483f1cbc47641
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2023-09-28 21:42:42 +0900

    Create solution.py

---
+++ Programmers/lev1/행렬의 덧셈/solution.py
@@ -0,0 +1,8 @@
+#solution 1
+def solution1(arr1,arr2):
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            arr1[i][j] += arr2[i][j]
+    return arr1
+                 
+
