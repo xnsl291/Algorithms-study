27475f3607edd387d7aa7de6f73a83b90d4cecea
hana cho <sjfn242@gmail.com>
2023-09-28 21:42:07 +0900

    update

--- Programmers/lev1/문자열을 정수로 바꾸기/solution.py
+++ Programmers/lev1/문자열을 정수로 바꾸기/solution.py
@@ -8,4 +8,14 @@ def solution(s):
 def sol2(s):
     return int(s)
 
+# another way : usage of sum & exponent
+def str2int(s): 
 
+    result = sum([ int(s[-i-1] ) *(10**i) for i in range(len(s)-1)])
+
+    if s[0].isdigit():#부호없음
+        result+= int(s[0])
+    elif s[0] == "-":
+        result *=-1
+    return result
+    
