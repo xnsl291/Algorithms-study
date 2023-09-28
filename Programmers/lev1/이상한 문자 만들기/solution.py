dfe2374fc25f5fcd299aba3ddbf5b2eb6c24ff37
hana cho <sjfn242@gmail.com>
2023-09-28 21:42:20 +0900

    add pg: 이상한 문자 만들기

---
+++ Programmers/lev1/이상한 문자 만들기/solution.py
@@ -0,0 +1,23 @@
+def solution(s):
+    ans = []
+    for a in s.split(" "):
+        tmp = "".join([a[l].lower() if l %2 !=0 else a[l].upper() for l in  range(len(a))])
+        ans.append(tmp)
+    return " ".join(ans)
+
+
+#enumerate, map 이용
+def solution2(s):
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
+
+
+s = "try hello world"
+expectation = "TrY HeLlO WoRlD"
+
+
+print(solution(s) )
+
+for a,b in enumerate(s.split(" ")):
+    print(a,b)
+# print(solution(s) == expectation)
+
