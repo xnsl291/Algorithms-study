2de957c2b3973bfb495d1a51e49e0f7d62f0dcb6
hana cho <sjfn242@gmail.com>
2023-09-28 21:45:06 +0900

    modify bj:java 2480_주사위_세개

--- BaekJoon/java/Bronze/2480_주사위_세개/solution_2480.java
+++ BaekJoon/java/Bronze/2480_주사위_세개/solution_2480.java
@@ -1,15 +1,3 @@
-// from collections import Counter
-// numbers = Counter(list(map(int,input().split())))
-
-// def solv(numbers):
-//     if len(numbers.keys())==1:
-//         return 10000+list(numbers.keys())[0]*1000
-//     elif len(numbers.keys())==2:
-//         return 1000+(numbers.most_common()[0][0])*100
-//     else:
-//         return (max(numbers.items())[0])*100
-
-// print(solv(numbers))
 package JAVA.pg_bronze.주사위_세개_2480;
 import java.util.Scanner;
 public class solution_2480
