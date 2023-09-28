b4de035903bfa9a52ef1905db3e5de816fe0879f
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2023-09-28 21:42:28 +0900

    Update 김서방찾기.py

--- Programmers/lev1/김서방찾기/김서방찾기.py
+++ Programmers/lev1/김서방찾기/김서방찾기.py
@@ -1,1 +1,11 @@
-s
+#solution 1 
+def solution(seoul):
+    for i in range(len(seoul)):
+        if "kim" in seoul:
+            idx = i
+    ans = (f"김서방은 {idx}에 있다")
+    return ans
+
+#solution 2 -> clean ver.
+def solution2(seoul):
+    return f"김서방은 {seoul.index('Kim')}에 있다")
