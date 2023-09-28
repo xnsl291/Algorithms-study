34b0a3eec53e8d49e36ba78797e8c26815dcb7e3
xnsl291 <110678557+xnsl291@users.noreply.github.com>
2023-09-28 21:42:35 +0900

    Update solution.py
    sqrt, pow 사용하기

--- Programmers/lev1/정수 제곱근 판별/solution.py
+++ Programmers/lev1/정수 제곱근 판별/solution.py
@@ -2,5 +2,19 @@
 def solution(n):
     return (n**0.5+1)**2 if int(n**0.5) == (n**0.5) else -1
   
+#solution2
+#use math library 
+
+from math import sqrt
+def solution(n):
+    return (sqrt(n)+1)**2 if sqrt(n)%1==0 else -1
+
+#solution3
+#math library, pow function
+
+from math import sqrt
+def solution(n):
+    return pow(sqrt(n)+1,2) if sqrt(n)%1==0 else -1
+
 
 
