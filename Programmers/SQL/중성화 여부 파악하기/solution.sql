9bc465f211f41cee608ab7280ffd4c6657ec67e2
hana cho <sjfn242@gmail.com>
2023-10-02 20:00:20 +0900

    add pg:sql 중성화 여부 파악하기.sql

---
+++ Programmers/SQL/중성화 여부 파악하기/solution.sql
@@ -0,0 +1,14 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59409
+
+-- SOLUTION1 
+SELECT ANIMAL_ID, NAME, 
+    CASE WHEN (SEX_UPON_INTAKE LIKE "Neutered %" OR SEX_UPON_INTAKE LIKE "Spayed %" ) THEN "O"
+    ELSE "X" END AS "중성화"
+FROM ANIMAL_INS 
+ORDER BY ANIMAL_ID;
+
+-- SOLUTION2
+SELECT ANIMAL_ID, NAME,
+       IF(SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%', 'O', 'X') AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
