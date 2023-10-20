5b648d44317e8c6eb7210b1ea3c91ec0b28b72a2
hana_home <sjfn242@gmail.com>
2023-10-20 15:05:44 +0900

    add pg:py 소수 찾기 - 다른사람 풀이 (*)

--- Programmers/py/lev1/소수 찾기/solution.py
+++ Programmers/py/lev1/소수 찾기/solution.py
@@ -9,3 +9,13 @@ def solution(n):
             num_lst[idx - 2] = 0 
             
     return sum(num_lst)
+
+
+# 다른사람 풀이 
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i)) # 배수 제거 
+    return len(num)
