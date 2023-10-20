0f74db3fa18e629e8708dbe0e00997b04af099a2
hana_home <sjfn242@gmail.com>
2023-10-20 15:03:42 +0900

    refacor pg:py 소수찾기 - 반복횟수 줄임

--- Programmers/py/lev1/소수 찾기/solution.py
+++ Programmers/py/lev1/소수 찾기/solution.py
@@ -1,13 +1,11 @@
 def solution(n):
     num_lst = [1 for i in range(2,n+1)] #2~n 
-    multifly_val = 2 
     
-    while(multifly_val <= n):
-        for i in range(2,n+1):
-            idx = i*multifly_val
+    for i in range(2,n):
+        for j in range(i,n+1):
+            idx = i*j
             if idx > n:
                 break
             num_lst[idx - 2] = 0 
-        multifly_val +=1 
             
     return sum(num_lst)
