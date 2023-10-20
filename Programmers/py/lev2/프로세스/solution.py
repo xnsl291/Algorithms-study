a072c6094a14bc7a808a0c7a109335f6a4f71dd4
hana_home <sjfn242@gmail.com>
2023-10-20 14:45:26 +0900

    modify pg:py 프로세스 - 다른사람 풀이 추가

--- Programmers/py/lev2/프로세스/solution.py
+++ Programmers/py/lev2/프로세스/solution.py
@@ -18,3 +18,18 @@ def solution(priorities, location):
                 break
             
     return order
+
+
+
+## 다른사람 풀이 
+def solution1(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
